73a4bffb390688f521ff117f2c95afe10613ea89
Theo Weller <weller.theodor@gmail.com>
2019-03-04 16:20:53 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,8 @@ def reformat_languages(languages)
 new_hash = {}
 new_hash[:ruby] = {:type => "interpreted"}
 new_hash[:ruby] = {:style => [:oo]}
-binding.pry
+
 end
 
 reformat_languages
+binding.pry
