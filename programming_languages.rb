f4aebdd6a8b16a6f715c5b995376207f9acef08c
Theo Weller <weller.theodor@gmail.com>
2019-03-04 16:33:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,16 +1,19 @@
 require 'pry'
-
 def reformat_languages(languages)
-new_hash = {}
-languages.each do |key, value|
-  value.each do |language, attribute_hash|
-    attribute_hash.each do |attribute_hash, data|
-    new_hash[language] = attribute
-  end
+  new_hash = {}
+  languages.each {
+    |style,style_hash|
+    style_hash.each {
+      |lang,lang_hash|
+      if new_hash.has_key?(lang)
+        new_hash[lang][:style] << style
+      else
+        new_hash[lang]=lang_hash
+        new_hash[lang][:style]=[style]
+      end
+    }
+  }
+  puts new_hash
+  new_hash
+  binding.pry
 end
-end
-end
-
-
-
-binding.pry
